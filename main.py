d624685ccb2554369ab95d7bafb523489a2836a5
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-02-06 00:39:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+database = "./data.txt"
+rows = ["isim","numarasi","sinifi"]
+
+def read(id, column=rows[0]):
+    with open(database) as dataread:
+        source = dataread.read().splitlines()
+    return source[id].split(",")[rows.index(column)]
+
+def add(string, data=""):
+    dict = {}
+    for s in [string]:
+        for sp in s.split(","):
+            key = sp.strip().split(":")[0]
+            value = sp.strip().split(":")[1]
+            dict [key] = value
+    for r in rows:
+        data = data   dict[r]   ","
+    with open(database,"a") as append:
+        append.write(data[0:-1] "\n")
+
+
+
+#add("isim:ahmet, sinifi:10K, numarasi:2")
+
+print read(1, "sinifi")
